3691792e8ff8d78e50f55cbdb1e60fa7c16d486e
gil-son <gilszon@gmail.com>
2021-06-02 09:21:09 -0300

    Initial project

---
+++ src/main/java/com/dschallenge/crud/DsBootcampChallengeCrudApplication.java
@@ -0,0 +1,13 @@
+package com.dschallenge.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsBootcampChallengeCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsBootcampChallengeCrudApplication.class, args);
+	}
+
+}
