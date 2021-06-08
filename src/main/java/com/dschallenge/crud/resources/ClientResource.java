8180040b00e7d7698757514c1a5617261ab9161f
gil-son <gilszon@gmail.com>
2021-06-08 09:40:54 -0300

    Client Resource

--- src/main/java/com/dschallenge/crud/resources/ClientResource.java
+++ src/main/java/com/dschallenge/crud/resources/ClientResource.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +25,22 @@ public class ClientResource {
 		List<ClientDTO> list = clientService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+		ClientDTO dto = clientService.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
